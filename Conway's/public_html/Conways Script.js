ec059f68e10e0757128af8d6ef53b25d1a62044b
Davis Moore <dcm1042@wildcats.unh.edu>
2021-04-03 19:24:06 -0400

    added helper functions Neighborcount and random

--- Conway's/public_html/Conways Script.js
+++ Conway's/public_html/Conways Script.js
@@ -9,18 +9,15 @@ var ctx = c.getContext("2d");
 var boardwidth = 60;
 var boardheight = 60;
 var gameOn = false;
-var board = [];
+var board= [];
 
 ctx.font = "30px Arial";
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function test(){
-    
-    while(true){
-
-        var board = [];
+function random(gameboard){
+        gameboard = [];
 
         var faceX = 0;
         ctx.clearRect(0, 0, c.width, c.height);
@@ -30,21 +27,29 @@ async function test(){
                 for(let j = 0; j < boardheight; j++){
                     sample.push(Math.round(Math.random()));
                 }
-            board.push(sample);
+            gameboard.push(sample);
             
-            } 
+            }
+        return gameboard;
+}
+async function test(){
+    
+    while(true){
 
-
+        board = random(board);
+        
         for(var x = 0; x < boardwidth; x++){
-            //console.log("pain"+x.toString())
+            
             for(var y = 0; y < boardheight; y++){  
+                
                         if(board[x][y] === 0){
                            ctx.beginPath();
                            ctx.fillRect(x*(c.width/boardwidth), y*(c.height/boardheight), c.width/boardwidth, c.height/boardheight); 
                             //ctx.rect(x*10, y*10, c.height/boardheight, c.width/boardwidth);
                            ctx.stroke();
+                           
                         }
-                        
+                     
                         
                     }
                 }
@@ -56,7 +61,19 @@ async function test(){
 
 test();
 
-
+function NeighborCount(x,y ,board){
+        var counter = 0;
+       
+        for(let row=-1;row<2;row++){
+            for(let col = -1;col<2;col++){
+                if ((x+row>=0)&& (y + col>=0 )&& (x+row<boardwidth)&& (y + col<boardheight ) && !(row==0&&col==0)){
+                     counter += board[x+row][y+col];
+                    }               
+            }
+        }    
+        
+        return counter;
+    } 
 
 
 
