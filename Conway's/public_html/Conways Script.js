77a441d331c66f9137558a4b6c7e4909e79ba1cd
Davis Moore <dcm1042@wildcats.unh.edu>
2021-04-03 18:35:05 -0400

    made a board that generates a random playfield

--- Conway's/public_html/Conways Script.js
+++ Conway's/public_html/Conways Script.js
@@ -6,11 +6,57 @@
 
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.font = "30px Arial";
-ctx.fillText("Hello World", 10, 50);
+var boardwidth = 60;
+var boardheight = 60;
+var gameOn = false;
+var board = [];
 
-ctx.stroke();
+ctx.font = "30px Arial";
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function test(){
+    
+    while(true){
+
+        var board = [];
+
+        var faceX = 0;
+        ctx.clearRect(0, 0, c.width, c.height);
+        
+        for(let i = 0; i < boardwidth; i++){
+            let sample = [];
+                for(let j = 0; j < boardheight; j++){
+                    sample.push(Math.round(Math.random()));
+                }
+            board.push(sample);
+            
+            } 
+
+
+        for(var x = 0; x < boardwidth; x++){
+            //console.log("pain"+x.toString())
+            for(var y = 0; y < boardheight; y++){  
+                        if(board[x][y] === 0){
+                           ctx.beginPath();
+                           ctx.fillRect(x*(c.width/boardwidth), y*(c.height/boardheight), c.width/boardwidth, c.height/boardheight); 
+                            //ctx.rect(x*10, y*10, c.height/boardheight, c.width/boardwidth);
+                           ctx.stroke();
+                        }
+                        
+                        
+                    }
+                }
+
+               await sleep(1000);
+         
+        }
+    }   
+
+test();
+
+
+
 
 
